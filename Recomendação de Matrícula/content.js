1ea48bd0827c03635ecec9e450b70a5849788c5a
Wagner Martins <wagnermc506@gmail.com>
2019-09-20 12:07:56 -0300

    Leitura do nome e curso no quadro resumo
    Co-authored-by: Antonio Ruan <ruanmoura13@outlook.com>

---
+++ Recomendação de Matrícula/content.js
@@ -0,0 +1,7 @@
+var option = document.getElementById("lblOpcOpcao").innerText
+var student = document.getElementById("lblAluNome").innerText 
+var curse = document.getElementById("lblOpcDenominacao").innerText
+
+console.log("Opção de curso: " + option)
+console.log("Nome do curso: " + curse)
+console.log("Nome do aluno: " + student)
